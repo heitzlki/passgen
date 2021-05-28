f929ad27088f4a4fc5c47428c7058296caa92803
heitzlki <kirill.heitzler@gmail.com>
2021-05-28 11:33:02 +0200

    ✨ create own version of passgen

--- passgen.py
+++ passgen.py
@@ -1,15 +1,1 @@
-import random
-chars = ("abcdefghijklmnopqrstuvwxyz1234567890")
-try:
-    anzahl = int(input("Wie viel Passwörter willst du haben?"))
-except:
-    anzahl = 1
-    print("Variable muss eine Zahl sein... autoassigned: 1")
-try:
-    länge = int(input("passwort länge ?"))
-except:
-    länge = 13
-    print("Länge muss eine Zahl sein, autoassigned: 13")
-for i in range(anzahl):
-  passwort ="".join(random.choices(chars,k = länge))
-  print(passwort)
+import random;a,l=int(input("Amount:") or "1"), int(input("Length:") or "12");[print ("".join(random.choices("abcdefghijklmnopqrstuvwxyz1234567890",k=l))) for _ in range(a)]
